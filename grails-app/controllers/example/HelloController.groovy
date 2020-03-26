f00c4305d281273f61c579c9f29bc1b99c8d43fd
Puneet Behl <behl4485@gmail.com>
2020-03-26 20:11:17 +0530

    Implemented send mail logic

--- grails-app/controllers/example/HelloController.groovy
+++ grails-app/controllers/example/HelloController.groovy
@@ -1,9 +1,22 @@
 package example
 
+import grails.plugins.mail.MailService
+
 class HelloController {
 	static responseFormats = ['json', 'xml']
-	
+
+    MailService mailService
+
     def index() {
-        respond "Hello World!"
+        respond model: [message: "Hello World!"]
+    }
+
+    def test() {
+        mailService.sendMail {
+            from "admin@mysystem.com"
+            subject "New user"
+            text "A new user has been created"
+        }
+        respond model: [message: "Mail sent!"]
     }
 }
